e54af48244b9853417c21f8f2228b495cfe365b3
Forddev777 <taothongdev@gmail.com>
2022-01-08 21:22:36 +0700

    Initial Commit

---
+++ dev-auto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dev-auto
+//
+//  Created by suriya taothongkom on 8/1/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
